4f02d7370908a58d9c2f35145987299e1f3ea779
Piyush <f20200114@goa.bits-pilani.ac.in>
2021-05-13 22:48:16 +0530

    added sky high+react-script dependencies

--- src/App.js
+++ src/App.js
@@ -16,6 +16,12 @@ function App() {
 			imageSrc: 'https://source.unsplash.com/random/201x201'
 		},
 		{
+			title: 'Sky High',
+			artist: 'Electronomia',
+			audioSrc: '/assets/sh.mp3',
+			imageSrc: 'https://linkstorage.linkfire.com/medialinks/images/27a334a4-35f8-45f1-980f-6dc187a2132e/artwork-440x440.jpg'
+		},
+		{
 			title: 'Another World',
 			artist: 'Vybloom',
 			audioSrc: '/assets/aw.mp3',
