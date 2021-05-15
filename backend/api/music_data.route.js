061b9c63ef461f4cd4528c3ad797ba3dce25b18b
Darahaas Yajamanyam <darahaas15@gmail.com>
2021-05-15 15:57:19 +0530

    Update music_data.route.js

--- backend/api/music_data.route.js
+++ backend/api/music_data.route.js
@@ -2,6 +2,6 @@ import express from 'express'
 
 const router = express.Router()
 
-router.route('/').get((req, res) =>res.send("hello world"))
+router.route('/').get((req, res) =>res.send("JamPodd"))
 
 export default router
