88607c96d49917fd35c0766abdfadd203f9f46e6
ashmitkx <ashmitk0507@gmail.com>
2021-05-15 20:40:58 +0530

    added google cdn links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,33 +15,33 @@ function App() {
 		{
 			title: 'Detergent',
 			artist: 'Dylan Sitts',
-			audioSrc: '/assets/det.mp3',
+			audioSrc: 'http://130.211.5.196/det.mp3',
 			imageSrc:
 				'https://d34qmkt8w5wll9.cloudfront.net/commercial-releases/cover_art/jpeg/3730.jpg'
 		},
 		{
 			title: 'Hot Heat',
 			artist: 'Youtube Audio Library',
-			audioSrc: '/assets/hh.mp3',
+			audioSrc: 'http://130.211.5.196/hh.mp3',
 			imageSrc: 'https://source.unsplash.com/random/200x200'
 		},
 		{
 			title: 'Waiting For Nothing',
 			artist: 'Day Wave',
-			audioSrc: '/assets/wfn.mp3',
+			audioSrc: 'http://130.211.5.196/wfn.mp3',
 			imageSrc: 'https://source.unsplash.com/random/201x201'
 		},
 		{
 			title: 'Sky High',
 			artist: 'Electronomia',
-			audioSrc: '/assets/sh.mp3',
+			audioSrc: 'http://130.211.5.196/sh.mp3',
 			imageSrc:
 				'https://linkstorage.linkfire.com/medialinks/images/27a334a4-35f8-45f1-980f-6dc187a2132e/artwork-440x440.jpg'
 		},
 		{
 			title: 'Another World',
 			artist: 'Vybloom',
-			audioSrc: '/assets/aw.mp3',
+			audioSrc: 'http://130.211.5.196/aw.mp3',
 			imageSrc: 'https://source.unsplash.com/random/199x199'
 		}
 	];
