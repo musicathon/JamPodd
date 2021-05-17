acdff906edeaa1ef81636cc3b5ded26333b59982
ashmitkx <ashmitk0507@gmail.com>
2021-05-17 12:04:40 +0530

    added login page

--- frontend/src/components/LoginPage/LoginPage.js
+++ frontend/src/components/LoginPage/LoginPage.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import './LoginPage.css';
 import { useGoogleLogin } from 'react-google-login';
-import { FcGoogle } from 'react-icons/fc';
+import { AiOutlineGoogle } from 'react-icons/ai';
 
 // refresh token
 import { refreshTokenSetup } from '../../utils/refreshToken';
@@ -8,6 +9,14 @@ import { refreshTokenSetup } from '../../utils/refreshToken';
 const clientId = process.env.REACT_APP_GOOGLE_OAUTH_ID;
 
 function LoginPage({ setIsAuth }) {
+	// transition on mount
+	const [isShown, setIsShown] = useState(false);
+	useEffect(() => {
+		setIsShown(true);
+
+		return () => setIsShown(false);
+	}, []);
+
 	const onSuccess = (res) => {
 		setIsAuth(true);
 		console.log('Login Success: currentUser:', res.profileObj);
@@ -26,10 +35,21 @@ function LoginPage({ setIsAuth }) {
 	});
 
 	return (
-		<button onClick={signIn} className='button'>
-			<FcGoogle />
-			<span className='buttonText'>Sign in with Google</span>
-		</button>
+		<article className='login'>
+			<div className={`login__cntr ${isShown ? '--shown' : ''}`}>
+				<h1>JamPodd</h1>
+				<h1>Sign In</h1>
+				<div className='login__img-cntr'>
+					<img src='/logo-dark.png' alt='jampodd logo' />
+				</div>
+				<button onClick={signIn} className='login__btn'>
+					<div className='login__icon-cntr'>
+						<AiOutlineGoogle />
+					</div>
+					<span>BITS Mail</span>
+				</button>
+			</div>
+		</article>
 	);
 }
 
