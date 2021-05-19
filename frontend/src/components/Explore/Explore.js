4ddbddfc8a24c2886afc65712cf429a73b11e1a2
ashmitkx <ashmitk0507@gmail.com>
2021-05-19 17:45:48 +0530

    connected explore page to backend

--- frontend/src/components/Explore/Explore.js
+++ frontend/src/components/Explore/Explore.js
@@ -1,92 +1,28 @@
 import './Explore.css';
+import songsDS from '../../utils/songsDS';
 import { useState } from 'react';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { IoMdAdd } from 'react-icons/io';
 import { IoPlay, IoClose } from 'react-icons/io5';
 
 const Explore = ({ setCurrentTrackIndex, setCurrentTrackList }) => {
-	// get from backend
-	const tracks = [
-		{
-			title: 'Detergent',
-			artist: 'Dylan Sitts',
-			album: 'Venture',
-			duration: { min: 6, sec: 5 },
-			audioSrc: 'http://130.211.5.196/det.mp3',
-			imageSrc:
-				'https://d34qmkt8w5wll9.cloudfront.net/commercial-releases/cover_art/jpeg/3730.jpg'
-		},
-		{
-			title: 'Hot Heat',
-			artist: 'Topher Mohr',
-			album: 'Youtube Audio Library',
-			duration: { min: 3, sec: 24 },
-			audioSrc: 'http://130.211.5.196/hh.mp3',
-			imageSrc: 'https://source.unsplash.com/random/200x200'
-		},
-		{
-			title: 'Waiting For Nothing',
-			artist: 'Day Wave',
-			album: 'Waiting For Nothing',
-			duration: { min: 2, sec: 56 },
-			audioSrc: 'http://130.211.5.196/wfn.mp3',
-			imageSrc: 'https://source.unsplash.com/random/201x201'
-		},
-		{
-			title: 'Sky High',
-			artist: 'Electronomia',
-			album: 'NCS Release',
-			duration: { min: 5, sec: 24 },
-			audioSrc: 'http://130.211.5.196/sh.mp3',
-			imageSrc:
-				'https://linkstorage.linkfire.com/medialinks/images/27a334a4-35f8-45f1-980f-6dc187a2132e/artwork-440x440.jpg'
-		},
-		{
-			title: 'Another World',
-			artist: 'Vybloom',
-			album: 'No Limits',
-			duration: { min: 4, sec: 20 },
-			audioSrc: 'http://130.211.5.196/aw.mp3',
-			imageSrc: 'https://source.unsplash.com/random/199x199'
-		}
-	];
-
-	const playlists = [
-		{
-			title: 'Indie',
-			songCount: 65,
-			imageSrc: 'https://source.unsplash.com/random/'
-		},
-		{
-			title: 'Truth or Consequences',
-			songCount: 9,
-			imageSrc:
-				'https://media.pitchfork.com/photos/5e6bb1b2690af00008f803fa/1:1/w_600/Truth%20or%20Consequences_Yumi%20Zouma.jpg'
-		},
-		{
-			title: 'Safety',
-			songCount: 6,
-			imageSrc:
-				'https://i1.sndcdn.com/artworks-vXenm8RPv3AE8grN-5tyRHQ-t500x500.jpg'
-		},
-		{
-			title: 'lofi',
-			songCount: 47,
-			imageSrc:
-				'https://is2-ssl.mzstatic.com/image/thumb/Music124/v4/eb/09/35/eb0935a7-d940-2583-51ca-ed4007752e3b/cover.jpg/400x400bb.jpeg'
-		},
-		{
-			title: 'Attack on Titan',
-			songCount: 7,
-			imageSrc: 'https://wallpaperaccess.com/full/279058.jpg'
-		}
-	];
-
+	const [queriedTracks, setQueriedTracks] = useState(); // server response
 	const [query, setQuery] = useState('');
+
 	const [checkedSongs, setCheckedSongs] = useState({});
 	const [isAnySongChecked, setIsAnySongChecked] = useState(false);
 
+	const [playlists, setPlaylists] = useState();
 	const [showPlSelector, setShowPlSelector] = useState(false);
+
+	const onSearch = async (e) => {
+		e.preventDefault();
+
+		await songsDS
+			.getByKeyword(query)
+			.then((res) => setQueriedTracks(res.data.songs))
+			.catch((e) => console.error(e));
+	};
 
 	const onCheck = (e) => {
 		const newObj = checkedSongs;
@@ -102,16 +38,85 @@ const Explore = ({ setCurrentTrackIndex, setCurrentTrackList }) => {
 	};
 
 	const onPlay = (index) => {
-		setCurrentTrackList(tracks);
+		setCurrentTrackList(queriedTracks);
 		setCurrentTrackIndex(index);
 	};
+
+	let searchResults;
+	if (queriedTracks) {
+		// If server actually responded with a tracks array
+		if (queriedTracks.length > 0) {
+			// If the tracks array has any results
+			searchResults = (
+				<>
+					<div className='addtopl'>
+						<span className='addtopl__prompt'>
+							Select Songs to Add to a Playlist
+						</span>
+						<button
+							type='button'
+							className='btn --submit'
+							disabled={isAnySongChecked ? false : true}
+							onClick={() => setShowPlSelector(true)}
+						>
+							<IoMdAdd />
+							<span>Add to Playlist</span>
+						</button>
+					</div>
+					<div className='results'>
+						{queriedTracks.map((track, index) => (
+							<div className={`songfull`} key={index}>
+								<div className='songfull__cntr songsmall'>
+									<div className='songsmall__img-cntr'>
+										<img src={track.imageSrc} alt='song art' />
+										<button
+											type='button'
+											className='songfull__btn --play'
+											onClick={() => onPlay(index)}
+										>
+											<IoPlay />
+										</button>
+									</div>
+
+									<div className='songsmall__info'>
+										<span className='songsmall__name'>
+											{track.title}
+										</span>
+										<span className='songsmall__artist'>
+											{track.artist}
+										</span>
+									</div>
+								</div>
+
+								<span className='songfull__cntr songfull__album'>
+									{track.album}
+								</span>
+
+								<input
+									className='songfull__cntr songfull__checkbox'
+									type='checkbox'
+									name={index}
+									onChange={onCheck}
+								/>
+							</div>
+						))}
+					</div>
+				</>
+			);
+		} else {
+			searchResults = <h2 className='error'>No Results Found</h2>;
+		}
+	} else {
+		// If the server failed to return an array of 0 or more results
+		searchResults = '';
+	}
 
 	return (
 		<>
 			<div className={`selectpl popup ${showPlSelector ? '--shown' : ''}`}>
 				<h2>Select a Playlist</h2>
 
-				{playlists.length > 0 ? (
+				{playlists ? (
 					<div className='selectpl__cntr'>
 						{playlists.map((playlist, index) => (
 							<button
@@ -148,7 +153,7 @@ const Explore = ({ setCurrentTrackIndex, setCurrentTrackList }) => {
 			>
 				<h4>Explore</h4>
 
-				<form onSubmit={(e) => e.preventDefault()} className='search'>
+				<form onSubmit={onSearch} className='search'>
 					<input
 						type='text'
 						onChange={(e) => setQuery(e.target.value)}
@@ -159,64 +164,7 @@ const Explore = ({ setCurrentTrackIndex, setCurrentTrackList }) => {
 					</button>
 				</form>
 
-				{tracks.length > 0 ? (
-					<>
-						<div className='addtopl'>
-							<span className='addtopl__prompt'>
-								Select Songs to Add to a Playlist
-							</span>
-							<button
-								type='button'
-								className='btn --submit'
-								disabled={isAnySongChecked ? false : true}
-								onClick={() => setShowPlSelector(true)}
-							>
-								<IoMdAdd />
-								<span>Add to Playlist</span>
-							</button>
-						</div>
-						<div className='results'>
-							{tracks.map((track, index) => (
-								<div className={`songfull`} key={index}>
-									<div className='songfull__cntr songsmall'>
-										<div className='songsmall__img-cntr'>
-											<img src={track.imageSrc} alt='song art' />
-											<button
-												type='button'
-												className='songfull__btn --play'
-												onClick={() => onPlay(index)}
-											>
-												<IoPlay />
-											</button>
-										</div>
-
-										<div className='songsmall__info'>
-											<span className='songsmall__name'>
-												{track.title}
-											</span>
-											<span className='songsmall__artist'>
-												{track.artist}
-											</span>
-										</div>
-									</div>
-
-									<span className='songfull__cntr songfull__album'>
-										{track.album}
-									</span>
-
-									<input
-										className='songfull__cntr songfull__checkbox'
-										type='checkbox'
-										name={index}
-										onChange={onCheck}
-									/>
-								</div>
-							))}
-						</div>
-					</>
-				) : (
-					<h2 className='error'>No Results Found</h2>
-				)}
+				{searchResults}
 			</main>
 		</>
 	);
